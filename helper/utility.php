4042e1f26bf8a16fdd3d3a72a645f9dd261b1f6a
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-06 15:06:26 +0100

    some helpers

---
+++ helper/utility.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helper;
+
+class Utility {
+    public static function redirect($url) {
+        header("Location: " . $url);
+        exit();
+    }
+    
+    public static function sanitize($string) {
+        return filter_var($string, FILTER_SANITIZE_STRING);
+    }
+    
+    public static function getParam($name, $default = null) {
+        return isset($_GET[$name]) ? self::sanitize($_GET[$name]) : $default;
+    }
+    
+    public static function postParam($name, $default = null) {
+        return isset($_POST[$name]) ? self::sanitize($_POST[$name]) : $default;
+    }
+}
