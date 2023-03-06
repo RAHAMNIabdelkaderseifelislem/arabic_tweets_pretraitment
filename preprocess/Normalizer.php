2a2d43b36b3f75e43c60da67f5096960046f8bca
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-06 15:16:37 +0100

    normalizer code

---
+++ preprocess/Normalizer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tweets_pre\Preprocess;
+
+class Normalizer {
+    public function normalize($tweet) {
+        // Remove diacritics (tashkeel) from the tweet
+        $tweet = preg_replace('/\p{M}/u', '', $tweet);
+        
+        // Normalize characters (harakat)
+        $tweet = str_replace(['ٍ','ٌ','ً','َ','ُ','ِ','ّ','ْ'], '', $tweet);
+        $tweet = str_replace(['آ','أ','إ'], 'ا', $tweet);
+        $tweet = str_replace('ى', 'ي', $tweet);
+        $tweet = str_replace('ؤ', 'و', $tweet);
+        
+        return $tweet;
+    }
+}
