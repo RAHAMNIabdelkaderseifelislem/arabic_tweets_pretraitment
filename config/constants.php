427ac4fdf39427f8907cce1c8f7d290691c1ab69
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-07 20:48:02 +0100

    constants updated

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,18 @@
 <?php
-// Define regex patterns for emojis and hashtags
-define('EMOJI_PATTERN', '/[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}]/u');
-define('HASHTAG_PATTERN', '/#\w+/u');
-?>
+
+// Database constants
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tweets_pre');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+/*
+// Twitter API constants till now i have not used twitter api
+define('TWITTER_CONSUMER_KEY', 'your_consumer_key');
+define('TWITTER_CONSUMER_SECRET', 'your_consumer_secret');
+define('TWITTER_ACCESS_TOKEN', 'your_access_token');
+define('TWITTER_ACCESS_TOKEN_SECRET', 'your_access_token_secret');
+*/
+// Other constants
+define('APP_NAME', 'Tweets Preprocessing');
+define('APP_VERSION', '0.2.0');
+define('APP_URL', 'http://localhost/your-web-app/');
