d1fe96197e5061158af3e6d327cd525ef0404ac5
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-07 21:10:46 +0100

    index.php begining of website

--- index.php
+++ index.php
@@ -1,49 +1,19 @@
 <?php
 require_once 'config/constants.php';
+require_once 'helper/utility.php';
+require_once 'controller/TweetController.php';
 
-if (isset($_POST["submit"])) {
-	// Retrieve the uploaded file
-	$file = $_FILES["csv_file"]["tmp_name"];
 
-	// Parse the CSV data into an array
-	$data = array_map('str_getcsv', file($file));
+header('Content-Type: text/html; charset=UTF-8');
 
-	// Remove emojis and hashtags, and non-Arabic letters from the second column of each row
-	foreach ($data as &$row) {
-		// Remove emojis
-		$row = preg_replace(EMOJI_PATTERN, '', $row);
+// Get the CSV file path from the query string parameter
+$csvFile = isset($_GET['csv']) ? $_GET['csv'] : '';
 
-		// Remove hashtags
-		$row = preg_replace(HASHTAG_PATTERN, '', $row);
+// Initialize the TweetController and fetch tweets from the CSV file
+$tweetController = new TweetController();
+$tweets = $tweetController->fetchTweetsFromCSV($csvFile);
 
-		// Remove non-Arabic letters
-		$row = preg_replace('/[^\x{0600}-\x{06FF}]/u', ' ', $row);
-	}
+// Render the tweet list view
+include 'view/index.php';
 
-	// Generate an HTML table from the modified CSV data
-	echo '<table border="1"><thead><tr><th>tweet</th></tr></thead>';
-	foreach ($data as $row) {
-		echo "<tr>";
-		foreach ($row as $cell) {
-			echo "<td>" . htmlspecialchars($cell, ENT_QUOTES, 'UTF-8') . "</td>";
-		}
-		echo "</tr>";
-	}
-	echo "</table>";
-}
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Pre-Processing of Dialectal Arabic Tweets</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-	<form method="post" enctype="multipart/form-data">
-		<input type="file" name="csv_file" accept=".csv">
-		<button type="submit" name="submit">Submit</button>
-	</form>
-</body>
-</html>
