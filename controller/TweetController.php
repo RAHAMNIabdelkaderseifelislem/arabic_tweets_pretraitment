5c59dde273f01650b4ef348403438ee3014c5078
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-07 20:57:33 +0100

    change the utility of tweet controller

--- controller/TweetController.php
+++ controller/TweetController.php
@@ -8,46 +8,49 @@ use tweets_pre\Preprocess\Normalizer;
 
 class TweetController {
     private $tweetModel;
-    private $tokenizer;
     private $normalizer;
+    private $tokenizer;
     
     public function __construct() {
         $this->tweetModel = new TweetModel();
-        $this->tokenizer = new Tokenizer();
         $this->normalizer = new Normalizer();
+        $this->tokenizer = new Tokenizer();
     }
     
     public function index() {
         // Get all tweets from the database
         $tweets = $this->tweetModel->getAllTweets();
         
-        // Pass tweets to the view for display
-        include_once 'view/index.php';
+        // Load the view to display the tweets
+        include('view/index.php');
     }
     
-    public function preprocess() {
-        // Get all tweets from the database
-        $tweets = $this->tweetModel->getAllTweets();
+    public function create() {
+        // Load the view to display the tweet creation form
+        include('view/preprocessing.php');
+    }
+    
+    public function store() {
+        // Get the input tweet from the form
+        $inputTweet = $_POST['tweet'];
         
-        // Preprocess each tweet
-        foreach ($tweets as $tweet) {
-            // Tokenize tweet into words
-            $words = $this->tokenizer->tokenize($tweet['text']);
-            
-            // Normalize each word in the tweet
-            $normalized_words = array();
-            foreach ($words as $word) {
-                $normalized_word = $this->normalizer->normalize($word);
-                array_push($normalized_words, $normalized_word);
-            }
-            
-            // Update the tweet in the database with the preprocessed text
-            $this->tweetModel->updateTweet($tweet['id'], implode(' ', $normalized_words));
-        }
+        // Normalize and tokenize the tweet
+        $normalizedTweet = $this->normalizer->normalize($inputTweet);
+        $tokenizedTweet = $this->tokenizer->tokenize($normalizedTweet);
         
-        // Redirect to the index page
+        // Save the tokenized tweet to the database
+        $this->tweetModel->createTweet($tokenizedTweet);
+        
+        // Redirect back to the index page
+        header('Location: ' . APP_URL);
+    }
+    
+    public function delete($id) {
+        // Delete the tweet with the given ID from the database
+        $this->tweetModel->deleteTweet($id);
+        
+        // Redirect back to the index page
         header('Location: ' . APP_URL);
     }
 }
-
 ?>
