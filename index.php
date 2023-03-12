7d5c07b04038c6fb96b5b7b5387b0439cf933e79
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-12 20:57:15 +0100

    delete non arabic letters and show both before and after

--- index.php
+++ index.php
@@ -1,49 +1,81 @@
 <?php
-require_once 'config/constants.php';
+
+$original_data = null;
+$processed_data = null;
 
 if (isset($_POST["submit"])) {
-	// Retrieve the uploaded file
-	$file = $_FILES["csv_file"]["tmp_name"];
+    // Retrieve the uploaded file
+    $file = $_FILES["csv_file"]["tmp_name"];
 
-	// Parse the CSV data into an array
-	$data = array_map('str_getcsv', file($file));
+    // Parse the CSV data into an array
+    $original_data = array_map('str_getcsv', file($file));
 
-	// Remove emojis and hashtags, and non-Arabic letters from the second column of each row
-	foreach ($data as &$row) {
-		// Remove emojis
-		$row = preg_replace(EMOJI_PATTERN, '', $row);
+    // Remove emojis and hashtags, and non-Arabic letters from the second column of each row
+    $processed_data = array();
+    foreach ($original_data as $row) {
 
-		// Remove hashtags
-		$row = preg_replace(HASHTAG_PATTERN, '', $row);
+        // Remove non-Arabic letters
+        $row = preg_replace('/[^\x{0600}-\x{06FF}]/u', ' ', $row);
 
-		// Remove non-Arabic letters
-		$row = preg_replace('/[^\x{0600}-\x{06FF}]/u', ' ', $row);
-	}
-
-	// Generate an HTML table from the modified CSV data
-	echo '<table border="1"><thead><tr><th>tweet</th></tr></thead>';
-	foreach ($data as $row) {
-		echo "<tr>";
-		foreach ($row as $cell) {
-			echo "<td>" . htmlspecialchars($cell, ENT_QUOTES, 'UTF-8') . "</td>";
-		}
-		echo "</tr>";
-	}
-	echo "</table>";
+        $processed_data[] = $row;
+    }
 }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pre-Processing of Dialectal Arabic Tweets</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pre-Processing of Dialectal Arabic Tweets</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>
+        function showOriginalTweets() {
+            document.getElementById("original_tweets").style.display = "block";
+            document.getElementById("processed_tweets").style.display = "none";
+        }
+        function showProcessedTweets() {
+            document.getElementById("original_tweets").style.display = "none";
+            document.getElementById("processed_tweets").style.display = "block";
+        }
+    </script>
 </head>
 <body>
-	<form method="post" enctype="multipart/form-data">
-		<input type="file" name="csv_file" accept=".csv">
-		<button type="submit" name="submit">Submit</button>
-	</form>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="csv_file" accept=".csv">
+        <button type="submit" name="submit">Submit</button>
+    </form>
+    <?php if ($original_data): ?>
+        <h2>Original Tweets</h2>
+        <button onclick="showOriginalTweets()">Show Original Tweets</button>
+        <table border="1" id="original_tweets">
+            <thead>
+                <tr>
+                    <th>tweet</th>
+                </tr>
+            </thead>
+            <?php foreach ($original_data as $row): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($row[0], ENT_QUOTES, 'UTF-8'); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php endif; ?>
+    <?php if ($processed_data): ?>
+        <h2>Preprocessed Tweets</h2>
+        <button onclick="showProcessedTweets()">Show Preprocessed Tweets</button>
+        <table border="1" id="processed_tweets" style="display: none;">
+            <thead>
+                <tr>
+                    <th>tweet</th>
+                </tr>
+            </thead>
+            <?php foreach ($processed_data as $row): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($row[0], ENT_QUOTES, 'UTF-8'); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php endif; ?>
 </body>
 </html>
