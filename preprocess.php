1af34ba75ed666d0d3486171aa466524a9cbda73
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-25 10:42:04 +0100

    save automatically without button

--- preprocess.php
+++ preprocess.php
@@ -12,19 +12,8 @@ if (isset($_POST["submit"])) {
 
     // Remove emojis and hashtags, and non-Arabic letters from the second column of each row
     $processed_data = array();
-    foreach ($original_data as $row) {
 
-        // Remove non-Arabic letters
-        $row = preg_replace('/[^\x{0600}-\x{06FF}]/u', ' ', $row);
-
-        $processed_data[] = $row;
-    }
-}
-
-if (isset($_POST["save"])) {
-    // Retrieve the processed data
-    $processed_data = json_decode($_POST["processed_data"], true);
-
+    // preparing the array for the processed data to be saved in a csv file
     // Create the uploads directory if it doesn't exist
     if (!is_dir("uploads")) {
         mkdir("uploads");
@@ -32,14 +21,17 @@ if (isset($_POST["save"])) {
 
     // Save the processed data to a file
     $file = fopen("uploads/tweets-ar.csv", "w");
-    foreach ($processed_data as $row) {
-        fputcsv($file, $row);
-    }
-    fclose($file);
 
-    // Redirect to the original page with a success message
-    header("Location: preprocess.php?success=1");
-    exit;
+    foreach ($original_data as $row) {
+
+        // Remove non-Arabic letters
+        $row = preg_replace('/[^\x{0600}-\x{06FF}]/u', ' ', $row);
+
+        fputcsv($file, $row);
+        $processed_data[] = $row;
+    }
+
+    fclose($file);
 }
 
 ?>
@@ -94,22 +86,6 @@ if (isset($_POST["save"])) {
             <?php endforeach; ?>
         </table>
         <p><a href="uploads/tweets-ar.csv" download>Download processed data</a></p>
-    <?php endif; ?>
-    <?php if (!$processed_data && isset($_GET["success"])): ?>
-        <p>Data has been processed and saved successfully!</p>
-    <?php endif; ?>
-    <h2>Upload CSV file</h2>
-    <form method="post" enctype="multipart/form-data">
-        <input type="file" name="csv_file">
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-    <?php if ($processed_data): ?>
-        <h2>Save Preprocessed Data</h2>
-        <form method="post">
-            <input type="hidden" name="processed_data" value="<?php echo htmlspecialchars(json_encode($processed_data), ENT_QUOTES, 'UTF-8'); ?>">
-            <input type="submit" name="save" value="Save Preprocessed Data">
-        </form>
     <?php endif; ?>
 </body>
 </html>
