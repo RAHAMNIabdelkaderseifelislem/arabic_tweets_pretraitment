a63dbab14d192a3e298166ac63a94cfc049f5375
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2023-03-29 14:36:30 +0100

    debugging

--- pretraitment/tokenize.php
+++ pretraitment/tokenize.php
@@ -42,6 +42,14 @@ while(($line = fgetcsv($fp)) !== FALSE) {
 // close the files
 fclose($fp);
 fclose($fp2);
+echo $linecount;
+for ($i = 0; $i < $linecount-1; $i++) {
+    echo $num_words[$i];
+    foreach($lines[$i] as $word) {
+        echo $word.' ';
+    }
+    echo '<br>';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -82,7 +90,7 @@ fclose($fp2);
                         <button id="hide_btn" onclick="hideWords()" style="display: none;">Hide Words</button>
                         <div id="words" style="display: none;">
                             <?php foreach($lines[$i] as $word): ?>
-                                <?php echo htmlspecialchars($word, ENT_QUOTES, 'UTF-8').';'; ?><br>
+                                <?php echo $word.' '; ?><br>
                             <?php endforeach; ?>
                         </div>
                     </td>
